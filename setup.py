30fe3559809080f62bbaaf2944410b6673ee3ab0
Omer Akram <om26er@gmail.com>
2016-09-24 01:02:03 +0500

    Update release to 0.3.1

--- setup.py
+++ setup.py
@@ -33,14 +33,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-simple-login',
-    version='0.3',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GPL Version 3',
     description='A simple django app to make accounts creation a breeze.',
     long_description=README,
-    url='https://github.com/byteshaft/django-simplelogin',
-    download_url='https://github.com/byteShaft/django-simplelogin/tarball/0.3',
+    url='https://github.com/byteshaft/django-simple-login',
+    download_url='https://github.com/byteShaft/django-simple-login/tarball/'
+                 '0.3.1',
     author='Omer Akram',
     author_email='om26er@gmail.com',
     keywords=['django', 'accounts'],
