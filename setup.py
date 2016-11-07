445c74d9cc6ebcf32ed45236f22d83d513cc061f
Omer Akram <om26er@gmail.com>
2016-11-07 15:04:54 +0500

    Fix setup.py to only include the library

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-
-#
-# Simple Login
-# Copyright (C) 2016 byteShaft
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 import os
-from setuptools import (
-    find_packages,
-    setup,
-)
-
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -33,17 +9,19 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-simple-login',
-    version='0.5.1',
-    packages=find_packages(),
-    include_package_data=True,
-    license='GNU GPL Version 3',
-    description='A simple django app to make accounts creation a breeze.',
-    long_description=README,
+    version='0.6.0',
+    packages=['simple_login', 'simple_login.apps',
+              'simple_login.utils', 'simple_login.views',
+              'simple_login.models', 'simple_login.managers',
+              'simple_login.exceptions', 'simple_login.serializers'],
     url='https://github.com/byteshaft/django-simple-login',
-    download_url='https://github.com/byteShaft/django-simple-login/tarball/'
-                 '0.5.1',
+    license='GNU GPL Version 3',
     author='Omer Akram',
     author_email='om26er@gmail.com',
+    description='A simple django app to make accounts creation a breeze.',
+    long_description=README,
+    download_url='https://github.com/byteShaft/django-simple-login/tarball/'
+                 '0.6.0',
     keywords=['django', 'accounts'],
     classifiers=[],
     install_requires=['django', 'djangorestframework'],
