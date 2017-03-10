23e781c70a9d4072c581a506a7375f791298682c
Omer Akram <om26er@gmail.com>
2017-03-10 22:57:58 +0500

    generate a 6 digit otp instead of 5

--- simple_login/utils/randomizer.py
+++ simple_login/utils/randomizer.py
@@ -23,4 +23,4 @@ import random
 
 def generate_random_key():
     # Ensures the returned number is always 5 numbers long.
-    return random.randint(10000, 99999)
+    return random.randint(100000, 999990)
