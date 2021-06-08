f4beb23b1c1d4de4a7019d5a6df287ba5c949050
Omer Akram <om26er@gmail.com>
2021-06-08 23:41:12 +0500

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-simple-login',
-    version='0.8.2',
+    version='0.8.3',
     packages=['simple_login', 'simple_login.apps', 'simple_login.migrations',
               'simple_login.utils', 'simple_login.views',
               'simple_login.models', 'simple_login.managers',
@@ -20,7 +20,7 @@ setup(
     author_email='om26er@gmail.com',
     description='A simple django app to make accounts creation a breeze.',
     long_description=README,
-    download_url='https://github.com/byteShaft/django-simple-login/tarball/0.8.2',
+    download_url='https://github.com/byteShaft/django-simple-login/tarball/0.8.3',
     keywords=['django', 'accounts'],
     classifiers=[],
     install_requires=['django', 'djangorestframework', 'oauth2', 'requests'],
